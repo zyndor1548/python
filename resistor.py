ca12318b86702d2386f24552531fca13e526bb0d
Aromal <149249986+zyndor1548@users.noreply.github.com>
2024-11-28 19:31:52 +0530

    Give color code of resistor and the resistance will be given

---
+++ resistor.py
@@ -0,0 +1,37 @@
+print ("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+print ("\tResistor color coding decoder")
+print ("\n\n\n")
+color = []
+colornum = { "black" : "0", "brown" : "1", "red" : "2", "orange" : "3", "yellow" : "4" , "green" : "5", "blue" : "6", "violet" : "7" ,"grey" : "8" , "white" : "9" }
+colorraise = { "black" : "⁰", "brown" : "¹", "red" : "²", "orange" : "³", "yellow" : "⁴" , "green" : "⁵", "blue" : "⁶", "violet" : "⁷" ,"grey" : "⁸" , "white" : "⁹" }
+tolerance = {  "brown" : "+/-1%", "red" : "+/-2%", "orange" : "+/-3%", "yellow" : "+/-4%" , "green" : "+/-0.5%", "blue" : "+/-0.25%", "violet" : "+/-0.1%" ,"grey" : "+/-0.05%"  , "gold" : " +/- 5 %", "silver" : " +/- 10 %"}
+colorcode = ''
+for i in range (4):
+    while True:
+        colorin =  input (f"color of {i + 1} th line in resistor :")
+        if i < 3:
+            if colorin in colornum:
+                break
+            else:
+                print ("\nEnter a valid color code \n")
+        elif i < 4:
+            if colorin in colorraise:
+                break
+            else:
+                print ("\nEnter a valid color code \n")
+        else:
+             if colorin in tolerance:
+                break
+             else:
+                print ("\nEnter a valid tolarance color code\n")
+             
+    color.append(colorin)
+for i in range (4):
+    if i < 2:
+        colorcode += colornum[color[i]]
+    elif i == 2:
+        colorcode += f" x 10{colorraise[color[i]]}"
+    else:
+        colorcode += f"  {tolerance[color[i]]}"
+
+print ("\n\n\n\t\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nresistance = ",colorcode)
